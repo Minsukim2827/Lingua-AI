193db87c2cf829a2348ef843d2013dddc3f9e06d
minsukim2827 <m.kim006064@gmail.com>
2024-10-01 23:34:30 +1300

    added some new pages

---
+++ app/mytranslations/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyTranslationsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>My Translations</h1>
+            <p>Welcome to the My Translations page!</p>
+        </div>
+    );
+};
+
+export default MyTranslationsPage;
---
+++ app/translate/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TranslatePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Translate Page</h1>
+            <p>Welcome to the translation page!</p>
+        </div>
+    );
+};
+
+export default TranslatePage;
